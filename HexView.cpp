cdb93ad432a1a8c0c7d2b51f8774d2ac39f8622f
Sam Izzo <sam@horsedrawngames.com>
2017-07-03 14:00:53 +1000

    Draw hex and ascii file data.

--- HexView.cpp
+++ HexView.cpp
@@ -1,18 +1,78 @@
 #include "HexView.h"
 #include "Log.h"
+#include <stdio.h>
+#include <assert.h>
 
-HexView::HexView(ConsoleBuffer* consoleBuffer) : Window(consoleBuffer)
+HexView::HexView(ConsoleBuffer* consoleBuffer, const char* filename) : Window(consoleBuffer, filename)
 {
+    m_fp = 0;
+    m_buffer = 0;
+    m_offset = 0;
+    fopen_s(&m_fp, filename, "rb");
+    if (m_fp)
+    {
+        fseek(m_fp, 0, SEEK_END);
+        m_fileSize = ftell(m_fp);
+        fseek(m_fp, 0, SEEK_SET);
+    }
+}
+
+HexView::~HexView()
+{
+    if (m_fp)
+        fclose(m_fp);
 }
 
 void HexView::Draw()
 {
-    m_consoleBuffer->FillRect(0, 1, m_width, m_height, '.', FOREGROUND_RED);
+    m_consoleBuffer->FillRect(0, 1, m_width, m_height, ' ', FOREGROUND_RED);
+
+    if (!m_fp)
+        return;
+
+    assert(m_buffer);
+    int offset = m_offset;
+
+    for (int j = 0; j < m_height; j++)
+    {
+        int y = 1 + j;
+        int x = 1;
+        m_consoleBuffer->Write(1, y, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY, "%08X", offset);
+        x += 9;
+
+        for (int i = 0; i < 16; i++, offset++)
+        {
+            char c = m_buffer[offset];
+
+            int xx = x + (i * 3);
+            m_consoleBuffer->Write(xx, y, FOREGROUND_GREEN, "%02X", c);
+
+            xx = x + (16 * 3) + i;
+            if (!isprint(c))
+                c = '.';
+            m_consoleBuffer->Write(xx, y, FOREGROUND_GREEN, "%c", c);
+        }
+    }
 }
 
-void HexView::OnWindowResize(int newWidth, int newHeight)
+void HexView::OnWindowResize(int width, int height)
 {
-    newHeight -= 2;
-    LogDebug("width=%i, height=%i\n", newWidth, newHeight);
-    Window::OnWindowResize(newWidth, newHeight);
+    height -= 2;
+    Window::OnWindowResize(width, height);
+    CacheFile();
+}
+
+void HexView::CacheFile()
+{
+    if (!m_fp)
+        return;
+
+    delete[] m_buffer;
+    //int numBytesToCache = m_offset 
+    m_buffer = new char[m_height * 16];
+    memset(m_buffer, 0, m_height * 16);
+
+    m_offset = 0;
+    fseek(m_fp, m_offset, SEEK_SET);
+    fread_s(m_buffer, m_height * 16, 1, m_height * 16, m_fp);
 }
--- HexView.h
+++ HexView.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include "Window.h"
+#include <stdio.h>
 
 class HexView : public Window
 {
     public:
-        HexView(ConsoleBuffer* consoleBuffer);
+        HexView(ConsoleBuffer* consoleBuffer, const char* filename);
+        ~HexView();
+
         virtual void Draw();
         virtual void OnWindowResize(int newWidth, int newHeight);
+
+    private:
+        void CacheFile();
+
+        FILE* m_fp;
+        char* m_buffer;
+        int m_offset;
+        int m_fileSize;
 };
