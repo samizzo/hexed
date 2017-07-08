fd775a45fb70ff2f5ad8c2822b9302f11eee00fa
Sam Izzo <sam@horsedrawngames.com>
2017-07-08 14:09:12 +1000

    Moved source files into subdirs.

--- source/ConsoleBuffer.cpp
+++ source/ConsoleBuffer.cpp
@@ -83,6 +83,7 @@ void ConsoleBuffer::Clear()
 
 void ConsoleBuffer::OnWindowResize(int width, int height)
 {
+    // TODO: Resize the window to the specified size.
     delete[] m_buffer;
     m_width = width;
     m_height = height;
