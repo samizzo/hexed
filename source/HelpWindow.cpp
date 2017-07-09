8b0e46ddc138f4194d38e325f70c513837078445
Sam Izzo <sam@horsedrawngames.com>
2017-07-09 12:59:59 +1000

    Draw colours in help window.

--- source/HelpWindow.cpp
+++ source/HelpWindow.cpp
@@ -4,6 +4,17 @@ void HelpWindow::OnWindowRefreshed()
 {
 	WORD colour = FOREGROUND_INTENSITY | BACKGROUND_GREEN;
 	s_consoleBuffer->DrawWindow(m_x, m_y, m_width, m_height, colour);
+
+	s_consoleBuffer->Write(m_x + 10, m_y + 8, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | BACKGROUND_GREEN, "Foreground");
+	s_consoleBuffer->Write(m_x + 30, m_y + 8, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | BACKGROUND_GREEN, "Background");
+	for (int i = 0; i < 16; i++)
+	{
+		int x = (m_x + 10) + ((i >> 3) * 10);
+		int y = m_y + 10 + (i & 7);
+		s_consoleBuffer->Write(x, y, i, "%c%c%c%c%c%c%c%c", 219, 219, 219, 219, 219, 219, 219, 219);
+
+		s_consoleBuffer->Write(x + 20, y, i << 4, "        ");
+	}
 }
 
 void HelpWindow::OnWindowResized(int width, int height)
